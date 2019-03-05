c895d22cfb35618d2855bfb58793c55d6aa55619
J-Ricker <lvlgrinddesign@gmail.com>
2019-03-05 14:51:54 -0500

    worked with find and findIndex to affect array data

--- array.js
+++ array.js
@@ -43,7 +43,29 @@ console.log({allAdults});
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+// const comment = comments.find(function (comment) {
+//     if (comment.id === 823423) {
+//         return true;
+//     }
+// });
+// console.log(comment);
+
+// same code above but with arrow function
+const comment = comments.find(comment => comment.id === 823423);
+console.log(comment);
+
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+
+const index = comments.findIndex(comment => comment.id === 823423);
+console.log(index);
+// can use comments.splice(index, 1);
+// also can use something popular in redux
+// creatign a new array
+
+const newComments = [
+    ...comments.slice(0, index),
+    ...comments.slice(index + 1)
+]
